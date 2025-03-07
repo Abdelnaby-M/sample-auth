93201e0672cb95ce4a665afc3acb324114ac3597
marwan <marwan.abdelnaby@paytabs.com>
2025-03-08 00:17:10 +0200

    upate e2e tests

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import 'module-alias/register';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,135 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import {
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { AppModule } from '../src/app.module';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('App e2e', () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3333);
+
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Auth', () => {
+    const signupDto: CreateUserDto = {
+      name: 'Jane',
+      email: 'jane_doe4@gmail.com',
+      password: '123456@a',
+    };
+
+    const signinDto = {
+      email: signupDto.email,
+      password: signupDto.password,
+    };
+
+    describe('POST /auth/signup', () => {
+      it('should throw if name is too short', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            name: 'Jo',
+            email: signupDto.email,
+            password: signupDto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if email is missing', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            name: signupDto.name,
+            password: signupDto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if password is missing', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            name: signupDto.name,
+            email: signupDto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should signup successfully', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(signupDto)
+          .expectStatus(201)
+          // .expectBodyContains(signupDto.email);
+      });
+    });
+
+    describe('POST /auth/signin', () => {
+      it('should throw if email is missing', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: signinDto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if password is missing', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: signinDto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should signin successfully', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(signinDto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
+      });
+    });
+
+    describe('GET /user/me', () => {
+      it('should get current user details', () => {
+        return pactum
+          .spec()
+          .get('/user/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200)
+          .expectBodyContains(signupDto.email);
+      });
+    });
   });
 });
